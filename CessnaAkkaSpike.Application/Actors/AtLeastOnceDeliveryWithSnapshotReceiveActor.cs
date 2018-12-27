1d3335da6e099a950fbd04b1f516d4ba694d2138
Geobarteam <gvd8@hotmail.com>
2018-12-27 11:19:20 +0100

    base class for AtLeastOnceDelivery

---
+++ CessnaAkkaSpike.Application/Actors/AtLeastOnceDeliveryWithSnapshotReceiveActor.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Akka.Actor;
+using Akka.Persistence;
+using CessnaAkkaSpike.Application.Messages;
+
+namespace CessnaAkkaSpike.Application.Actors
+{
+    public abstract class AtLeastOnceDeliveryWithSnapshotReceiveActorFilter<T> : AtLeastOnceDeliveryReceiveActor
+    {
+        public override string PersistenceId => Context.Self.Path.Name;
+        private int _counter = 0;
+
+        private ICancelable _recurringSnapshotCleanup;
+
+        private class CleanSnapshots { }
+
+        private readonly IActorRef[] _outports;
+
+        public AtLeastOnceDeliveryWithSnapshotReceiveActorFilter(IActorRef[] outports)
+        {
+            _outports = outports;
+
+            Recover<SnapshotOffer>(offer => offer.Snapshot is Akka.Persistence.AtLeastOnceDeliverySnapshot, offer =>
+            {
+                var snapshot = offer.Snapshot as Akka.Persistence.AtLeastOnceDeliverySnapshot;
+                SetDeliverySnapshot(snapshot);
+            });
+
+            Command<T>(message =>
+            {
+                HandleCommand(message);
+
+                _outports.ToList().ForEach(actor =>
+                    Deliver(actor.Path, messageId => new ReliableDeliveryEnvelope<T>(message, messageId)));
+
+                // save the full state of the at least once delivery actor
+                // so we don't lose any messages upon crash
+                SaveSnapshot(GetDeliverySnapshot());
+            });
+
+            Command<ReliableDeliveryAck>(ack =>
+            {
+                ConfirmDelivery(ack.MessageId);
+            });
+
+            Command<CleanSnapshots>(clean =>
+            {
+                // save the current state (grabs confirmations)
+                SaveSnapshot(GetDeliverySnapshot());
+            });
+
+            Command<SaveSnapshotSuccess>(saved =>
+            {
+                var seqNo = saved.Metadata.SequenceNr;
+                DeleteSnapshots(new SnapshotSelectionCriteria(seqNo, saved.Metadata.Timestamp.AddMilliseconds(-1))); // delete all but the most current snapshot
+            });
+
+            Command<SaveSnapshotFailure>(failure =>
+            {
+                ColorConsole.WriteLineRed(failure.ToString());
+            });
+        }
+
+        protected override void PreStart()
+        {
+            _recurringSnapshotCleanup =
+                Context.System.Scheduler.ScheduleTellRepeatedlyCancelable(TimeSpan.FromSeconds(10),
+                    TimeSpan.FromSeconds(10), Self, new CleanSnapshots(), ActorRefs.NoSender);
+
+            base.PreStart();
+        }
+
+        protected override void PostStop()
+        {
+            _recurringSnapshotCleanup?.Cancel();
+            base.PostStop();
+        }
+
+        protected abstract void HandleCommand(T message);
+    }
+}
--- CessnaAkkaSpike.Application/Actors/RegisterInstallerActor.cs
+++ CessnaAkkaSpike.Application/Actors/RegisterInstallerActor.cs
@@ -9,88 +9,21 @@ using CessnaAkkaSpike.Application.Repository;
 
 namespace CessnaAkkaSpike.Application.Actors
 {
-    public class RegisterInstallerActor : AtLeastOnceDeliveryReceiveActor
+    public class RegisterInstallerActor : AtLeastOnceDeliveryWithSnapshotReceiveActorFilter<PipelineMessage>
     {
-        private readonly IActorRef[] _outports;
         private readonly IRepository _repository;
 
-        public override string PersistenceId => Context.Self.Path.Name;
-        private int _counter = 0;
 
-
-        private ICancelable _recurringMessageSend;
-        private ICancelable _recurringSnapshotCleanup;
-
-        private class DoSend { }
-        private class CleanSnapshots { }
-
-        public RegisterInstallerActor(IActorRef[] Outports, IRepository repository)
+        public RegisterInstallerActor(IActorRef[] Outports, IRepository repository):base(Outports)
         {
-            _outports = Outports;
             _repository = repository;
-
-            Recover<SnapshotOffer>(offer => offer.Snapshot is Akka.Persistence.AtLeastOnceDeliverySnapshot, offer =>
-            {
-                var snapshot = offer.Snapshot as Akka.Persistence.AtLeastOnceDeliverySnapshot;
-                SetDeliverySnapshot(snapshot);
-            });
-
-           
-            Command<PipelineMessage>(message =>
-            {
-                ColorConsole.WriteMagenta($"{DateTime.Now} - Registring installer '{message.InstallerName}'");
-                _repository.CreateInstaller(message.InstallerName);
-
-                _outports.ToList().ForEach(actor =>
-                    Deliver(actor.Path, messageId => new ReliableDeliveryEnvelope<PipelineMessage>(message, messageId)));
-
-                // save the full state of the at least once delivery actor
-                // so we don't lose any messages upon crash
-                SaveSnapshot(GetDeliverySnapshot());
-            });
-
-            Command<ReliableDeliveryAck>(ack =>
-            {
-                ConfirmDelivery(ack.MessageId);
-            });
-
-            Command<CleanSnapshots>(clean =>
-            {
-                // save the current state (grabs confirmations)
-                SaveSnapshot(GetDeliverySnapshot());
-            });
-
-            Command<SaveSnapshotSuccess>(saved =>
-            {
-                var seqNo = saved.Metadata.SequenceNr;
-                DeleteSnapshots(new SnapshotSelectionCriteria(seqNo, saved.Metadata.Timestamp.AddMilliseconds(-1))); // delete all but the most current snapshot
-            });
-
-            Command<SaveSnapshotFailure>(failure =>
-            {
-                ColorConsole.WriteLineRed(failure.ToString());
-            });
-            
-            
         }
-        
-        protected override void PreStart()
+
+
+        protected override void HandleCommand(PipelineMessage message)
         {
-            _recurringSnapshotCleanup =
-                Context.System.Scheduler.ScheduleTellRepeatedlyCancelable(TimeSpan.FromSeconds(10),
-                    TimeSpan.FromSeconds(10), Self, new CleanSnapshots(), ActorRefs.NoSender);
-
-            base.PreStart();
+            ColorConsole.WriteMagenta($"{DateTime.Now} - Registring installer '{message.InstallerName}'");
+            _repository.CreateInstaller(message.InstallerName);
         }
-
-        protected override void PostStop()
-        {
-            _recurringSnapshotCleanup?.Cancel();
-            _recurringMessageSend?.Cancel();
-
-            base.PostStop();
-        }
-
-        
     }
 }
